c84c25a24938b76089f81ddf0628f08bd19a4b81
Sritejaswini123 <Sritejaswini1234@gmail.com>
2025-04-22 15:41:51 +0530

    second commit

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono! this is tejaswini ')
 })
 
-app.get('/users', (c) => {
-  return c.json([{ id: 1,
-     name: 'John' }])
-  
-}
-)
-app.get('/html', (c) => {
-  return c.html('<h1>this is tejaswini</h1>')
- 
-})
-app.get('/website', (c) => {
-  return c.redirect('https://www.google.com')
-})
-
-app.get('/website', (c) => {
-  return c.redirect('https://www.google.com')
-})
-
-app.get('/notfound' , (c) => {
-  return c.json({ message: 'Not Found' }, 404)
-})
 serve({
   fetch: app.fetch,
   port: 8080
